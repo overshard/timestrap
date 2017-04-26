2d5294bd5974c79569c4274accf0309936e67cf6
Isaac Bythewood <isaac@bythewood.me>
2017-04-26 07:17:05 +0000

    Auto-create a General task for all new timesheets created

--- core/models.py
+++ core/models.py
@@ -4,22 +4,10 @@ from __future__ import unicode_literals
 from datetime import date
 
 from django.db import models
-from django.contrib.auth.models import User
-
-
-class Timesheet(models.Model):
-    name = models.CharField(max_length=255)
-    complete = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ['-id']
-
-    def __str__(self):
-        return 'Timesheet: ' + self.name
 
 
 class Task(models.Model):
-    timesheet = models.ForeignKey(Timesheet, related_name='tasks')
+    timesheet = models.ForeignKey('Timesheet', related_name='tasks')
     name = models.CharField(max_length=255)
     complete = models.BooleanField(default=False)
 
@@ -30,9 +18,28 @@ class Task(models.Model):
         return 'Task: ' + self.name
 
 
+class Timesheet(models.Model):
+    name = models.CharField(max_length=255)
+    complete = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        created = False
+        if self.id:
+            created = True
+        super(Timesheet, self).save(*args, **kwargs)
+        if not created:
+            Task.objects.create(name='General', timesheet=self)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return 'Timesheet: ' + self.name
+
+
 class Entry(models.Model):
-    task = models.ForeignKey(Task, related_name='entries')
-    user = models.ForeignKey(User, related_name='entries')
+    task = models.ForeignKey('Task', related_name='entries')
+    user = models.ForeignKey('auth.User', related_name='entries')
     date = models.DateField(blank=True)
     duration = models.DurationField(blank=True)
     note = models.TextField(blank=True, null=True)
