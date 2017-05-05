7fb4c012dc4fe7148db2f6a2ad2bc5e875dba42a
Chris Wells <chris@chrxs.net>
2017-05-04 21:03:10 -0400

    Enable Django Model permissions for REST Framework.

--- timestrap/settings/base.py
+++ timestrap/settings/base.py
@@ -135,7 +135,7 @@ COMPRESS_OFFLINE = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.DjangoModelPermissions'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
