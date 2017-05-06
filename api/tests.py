242646c7933b89409b667fa842ef8a7d7017b926
Chris Wells <chris@chrxs.net>
2017-05-06 07:42:37 -0400

    Integrate permissions with API testing.

--- api/tests.py
+++ api/tests.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 from django.test import Client as HttpClient
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.core.management import call_command
 from django.apps import apps
 
@@ -27,18 +27,22 @@ class BrowseableApiTestCase(TestCase):
 
         self.c = HttpClient()
 
-        super_user = fake.simple_profile()
-        super_user_password = fake.password()
-        User.objects.create_user(super_user['username'],
-                                 super_user['mail'],
-                                 super_user_password,
-                                 is_superuser=True)
+        fake_user = fake.simple_profile()
+        fake_password = fake.password()
+        User.objects.create_user(fake_user['username'],
+                                 fake_user['mail'],
+                                 fake_password)
 
-        self.c.login(username=super_user['username'],
-                     password=super_user_password)
+        self.user = User.objects.get(username=fake_user['username'])
+        self.c.login(username=fake_user['username'],
+                     password=fake_password)
 
     def test_users_get(self):
         page = self.c.get('/api/users/')
+        self.assertEqual(page.status_code, 200)
+
+    def test_permission_get(self):
+        page = self.c.get('/api/permissions/')
         self.assertEqual(page.status_code, 200)
 
     def test_clients_get(self):
@@ -57,9 +61,20 @@ class BrowseableApiTestCase(TestCase):
         page = self.c.post('/api/clients/', {
             'name': fake.company()
         })
+        self.assertEqual(page.status_code, 403)
+
+        self.user.user_permissions.add(
+            Permission.objects.get(codename='add_client'))
+
+        page = self.c.post('/api/clients/', {
+            'name': fake.company()
+        })
         self.assertEqual(page.status_code, 201)
 
     def test_clients_post_unicode(self):
+        self.user.user_permissions.add(
+            Permission.objects.get(codename='add_client'))
+
         page = self.c.post('/api/clients/', {
             'name': 'Юникод'
         })
@@ -69,7 +84,17 @@ class BrowseableApiTestCase(TestCase):
         clients_page = self.c.get('/api/clients/')
         clients = clients_page.json()
 
-        page = self.c.post('/api/clients/', {
+        page = self.c.post('/api/projects/', {
+            'client': clients['results'][0]['url'],
+            'name': fake.job()
+        })
+
+        self.assertEqual(page.status_code, 403)
+
+        self.user.user_permissions.add(
+            Permission.objects.get(codename='add_project'))
+
+        page = self.c.post('/api/projects/', {
             'client': clients['results'][0]['url'],
             'name': fake.job()
         })
@@ -77,10 +102,13 @@ class BrowseableApiTestCase(TestCase):
         self.assertEqual(page.status_code, 201)
 
     def test_projects_post_unicode(self):
+        self.user.user_permissions.add(
+            Permission.objects.get(codename='add_project'))
+
         clients_page = self.c.get('/api/clients/')
         clients = clients_page.json()
 
-        page = self.c.post('/api/clients/', {
+        page = self.c.post('/api/projects/', {
             'client': clients['results'][0]['url'],
             'name': 'Юникод'
         })
@@ -92,6 +120,17 @@ class BrowseableApiTestCase(TestCase):
         projects = projects_page.json()
         users_page = self.c.get('/api/users/')
         users = users_page.json()
+
+        page = self.c.post('/api/entries/', {
+            'project': projects['results'][0]['url'],
+            'user': users['results'][0]['url'],
+            'duration': '1:30:00'
+        })
+
+        self.assertEqual(page.status_code, 403)
+
+        self.user.user_permissions.add(
+            Permission.objects.get(codename='add_entry'))
 
         page = self.c.post('/api/entries/', {
             'project': projects['results'][0]['url'],
