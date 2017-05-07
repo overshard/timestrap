9c27f4b1cd44c09edf053704068693ab11cbaaef
Chris Wells <chris@chrxs.net>
2017-05-07 10:06:34 -0400

    Refactor Selenium test method and check log in failure.
    After a bit of research, I have decided to rely on Selenium's implicitly_wait setting to check for RiotJS page changes. For login, I have set up base.html to provide an ID to the main content container based on the view name. This makes the check as simple as possible, code wise, and will hopefully be helpful with other tests and maybe even app functionality.

--- timestrap/tests.py
+++ timestrap/tests.py
@@ -6,6 +6,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import override_settings
 
 from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchElementException
 
 from faker import Factory
 
@@ -27,17 +28,34 @@ class SeleniumTests(StaticLiveServerTestCase):
 
         super(SeleniumTests, cls).setUpClass()
         cls.selenium = WebDriver()
-        cls.selenium.implicitly_wait(10)
+        cls.selenium.implicitly_wait(5)
 
     @classmethod
     def tearDownClass(cls):
         cls.selenium.quit()
         super(SeleniumTests, cls).tearDownClass()
 
+    def verifyById(self, id):
+        try:
+            self.selenium.find_element_by_id(id)
+            return True
+        except NoSuchElementException:
+            return False
+
     def test_login(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+
         username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys(self.user['username'])
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('incorrect password')
+        self.selenium.find_element_by_name('login').click()
+        self.assertFalse(self.verifyById('view-dashboard'))
+
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.clear()
         username_input.send_keys(self.user['username'])
         password_input = self.selenium.find_element_by_name("password")
         password_input.send_keys(self.user['password'])
         self.selenium.find_element_by_name('login').click()
+        self.assertTrue(self.verifyById('view-dashboard'))
