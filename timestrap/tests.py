ac75f01d4c4e8b1fd8707e8456ba1f952eb933ac
Chris Wells <chris@chrxs.net>
2017-05-07 11:02:18 -0400

    Add basic link checks (based on permissions).

--- timestrap/tests.py
+++ timestrap/tests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import override_settings
 
@@ -21,11 +21,8 @@ class SeleniumTests(StaticLiveServerTestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.user = fake.simple_profile()
-        cls.user['password'] = fake.password()
-        User.objects.create_user(cls.user['username'], cls.user['mail'],
-                                 cls.user['password'])
-
+        cls.profile = fake.simple_profile()
+        cls.profile['password'] = fake.password()
         super(SeleniumTests, cls).setUpClass()
         cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(5)
@@ -35,6 +32,21 @@ class SeleniumTests(StaticLiveServerTestCase):
         cls.selenium.quit()
         super(SeleniumTests, cls).tearDownClass()
 
+    def logIn(self):
+        self.user = User.objects.create_user(self.profile['username'],
+                                             self.profile['mail'],
+                                             self.profile['password'])
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element_by_name('username')
+        username_input.clear()
+        username_input.send_keys(self.profile['username'])
+        password_input = self.selenium.find_element_by_name('password')
+        password_input.clear()
+        password_input.send_keys(self.profile['password'])
+        self.selenium.find_element_by_name('login').click()
+        return self.verifyById('view-dashboard')
+
     def verifyById(self, id):
         try:
             self.selenium.find_element_by_id(id)
@@ -42,20 +54,54 @@ class SeleniumTests(StaticLiveServerTestCase):
         except NoSuchElementException:
             return False
 
+    def verifyByXpath(self, xpath):
+        try:
+            self.selenium.find_element_by_xpath(xpath)
+            return True
+        except NoSuchElementException:
+            return False
+
     def test_login(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
 
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys(self.user['username'])
-        password_input = self.selenium.find_element_by_name("password")
+        username_input = self.selenium.find_element_by_name('username')
+        username_input.send_keys(self.profile['username'])
+        password_input = self.selenium.find_element_by_name('password')
         password_input.send_keys('incorrect password')
         self.selenium.find_element_by_name('login').click()
         self.assertFalse(self.verifyById('view-dashboard'))
 
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.clear()
-        username_input.send_keys(self.user['username'])
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys(self.user['password'])
-        self.selenium.find_element_by_name('login').click()
-        self.assertTrue(self.verifyById('view-dashboard'))
+        self.assertTrue(self.logIn())
+
+    def test_clients(self):
+        self.logIn()
+        self.assertFalse(self.verifyByXpath('//a[@href="/clients/"]'))
+
+        self.user.user_permissions.add(
+            Permission.objects.get(codename='view_client'))
+
+        self.selenium.get(self.live_server_url)
+        self.assertTrue(self.verifyByXpath('//a[@href="/clients/"]'))
+
+        self.selenium.find_element_by_xpath('//a[@href="/clients/"]').click()
+        self.assertTrue(self.verifyById('view-clients'))
+
+    def test_entries(self):
+        self.logIn()
+        self.assertFalse(self.verifyByXpath('//a[@href="/entries/"]'))
+
+        self.user.user_permissions.add(
+            Permission.objects.get(codename='view_entry'))
+
+        self.selenium.get(self.live_server_url)
+        self.assertTrue(self.verifyByXpath('//a[@href="/entries/"]'))
+
+        self.selenium.find_element_by_xpath('//a[@href="/entries/"]').click()
+        self.assertTrue(self.verifyById('view-entries'))
+
+    def test_reports(self):
+        self.logIn()
+        self.assertTrue(self.verifyByXpath('//a[@href="/reports/"]'))
+
+        self.selenium.find_element_by_xpath('//a[@href="/reports/"]').click()
+        self.assertTrue(self.verifyById('view-reports'))
