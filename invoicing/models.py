09a24f686a8edb2a81106bc40de7131f13af4824
Isaac Bythewood <isaac@bythewood.me>
2017-05-07 06:41:43 +0000

    Starting models for invoicing

--- invoicing/models.py
+++ invoicing/models.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class InvoiceProfile(models.Model)
+    client = models.OneToOneField('timesheets.Client')
+    payment_id = models.CharField(max_length=255)
+
+
+class Invoice(models.Model):
+    invoice_profile = models.ForeignKey('invoicing.Client')
+    entries = models.ManyToManyField('timesheets.Entry')
+    hourly_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    total_billed = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_id = models.CharField(max_lenght=255)
