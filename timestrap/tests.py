82c907efdaf1da8ef8fed519d31785f93f20b747
Chris Wells <chris@chrxs.net>
2017-05-08 22:04:45 -0400

    Add tests for Entry add and edit.

--- timestrap/tests.py
+++ timestrap/tests.py
@@ -5,12 +5,15 @@ from django.contrib.auth.models import User, Permission
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import override_settings
 
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.firefox.webelement import FirefoxWebElement
 
 from pyvirtualdisplay import Display
 
 from faker import Factory
+
+from timesheets.models import Client, Project
 
 
 fake = Factory.create()
@@ -146,8 +149,7 @@ class SeleniumTests(StaticLiveServerTestCase):
         self.assertIn(
             'Project Changed',
             self.selenium.find_element_by_css_selector(
-                'project:first-of-type').text
-        )
+                'project:first-of-type').text)
 
     def test_entries(self):
         self.logIn()
@@ -161,6 +163,69 @@ class SeleniumTests(StaticLiveServerTestCase):
         self.selenium.find_element_by_css_selector(
             'a[href="/entries/"]').click()
         self.selenium.find_element_by_id('view-entries')
+
+        client = Client(name="Client", archive=False)
+        client.save()
+        project = Project(name="Project 1", client=client, archive=False)
+        project.save()
+        project = Project(name="Project 2", client=client, archive=False)
+        project.save()
+
+        # The <entries> tag only has three elements when there is no add form.
+        self.assertEqual(len(self.selenium.find_elements_by_css_selector(
+            'entries > *')), 3)
+        self.user.user_permissions.add(
+            Permission.objects.get(codename='add_entry'),
+            Permission.objects.get(codename='view_client'),
+            Permission.objects.get(codename='view_project'))
+        self.selenium.refresh()
+        self.selenium.find_element_by_class_name(
+            'select2-selection__arrow').click()
+        self.selenium.find_element_by_class_name(
+            'select2-search__field').send_keys('Project 2')
+        self.selenium.find_element_by_class_name(
+            'select2-search__field').send_keys(Keys.RETURN)
+        self.selenium.find_element_by_css_selector(
+            'entries form input[placeholder="Note"').send_keys('Note')
+        self.selenium.find_element_by_css_selector(
+            'entries form input[placeholder="0:00"').send_keys('0:35')
+        self.selenium.find_element_by_css_selector(
+            'entries form button').submit()
+        self.assertIn('Client\nProject 2\nNote\n0:35',
+                      self.selenium.find_element_by_css_selector('entry').text)
+
+        # The <entry> tag does not have an Edit button.
+        self.assertNotIn('Edit', self.selenium.find_element_by_css_selector(
+            'entry').text)
+        self.user.user_permissions.add(
+            Permission.objects.get(codename='change_entry'))
+        self.selenium.refresh()
+        self.selenium.find_element_by_id('entry-edit-menu').click()
+        # The second entry is the "Edit" link option. For some reason there are
+        # two <a> elements for each dropdown entry and the real one is the
+        # second one.
+        self.selenium.find_elements_by_css_selector(
+            '.dropdown-menu a:nth-of-type(2)')[1].click()
+        self.selenium.find_element_by_css_selector(
+            'entry .select2-selection__arrow').click()
+        self.selenium.find_element_by_class_name(
+            'select2-search__field').send_keys('Project 1')
+        self.selenium.find_element_by_class_name(
+            'select2-search__field').send_keys(Keys.RETURN)
+        self.selenium.find_element_by_css_selector(
+            'entry input[value="Note"]').clear()
+        self.selenium.find_element_by_css_selector(
+            'entry input[value="Note"]').send_keys('Changed note')
+        self.selenium.find_element_by_css_selector(
+            'entry input[value="0:35"]').clear()
+        self.selenium.find_element_by_css_selector(
+            'entry input[value="0:35"]').send_keys('1.5')
+        self.selenium.find_element_by_css_selector(
+            'entry button').click()
+        self.selenium.refresh()
+        self.assertIn('Client\nProject 1\nChanged note\n1:30',
+                      self.selenium.find_element_by_css_selector('entry').text)
+
 
     def test_reports(self):
         self.logIn()
