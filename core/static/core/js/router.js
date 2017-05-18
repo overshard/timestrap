107267198bd4ed7d5c643c9e9f9ceff211cb226f
Isaac Bythewood <isaac@bythewood.me>
2017-05-18 05:41:02 -0400

    Router improvements and bug fix for non-routed pages (temp fix)

--- core/static/core/js/router.js
+++ core/static/core/js/router.js
@@ -1,30 +1,42 @@
-// TODO: Cleanup routes into a single function
 riot.compile(function() {
+    // Route helpers
     let currentPage = null;
-    route('/entries..', function() {
-        if (currentPage) {
-            currentPage.unmount(true);
-        }
-        currentPage = riot.mount('div#main', 'entries')[0];
+    function goRoute(newPage) {
+        if (currentPage) currentPage.unmount(true);
+        currentPage = riot.mount('div#main', newPage)[0];
+    }
+    function getRiotUrl(tag) {
+        let configUrlName = tag.toUpperCase();
+        let url = timestrapConfig.CORE_URLS[configUrlName]
+        return url.substring(0, url.length-1) + '..';  // The `..` matches everything after
+    }
+
+    // Routes
+    route(getRiotUrl('entries'), function() {
+        goRoute('entries');
     });
-    route('/clients..', function() {
-        if (currentPage) {
-            currentPage.unmount(true);
-        }
-        currentPage = riot.mount('div#main', 'clients')[0];
+    route(getRiotUrl('clients'), function() {
+        goRoute('clients');
     });
-    route('/reports..', function() {
-        if (currentPage) {
-            currentPage.unmount(true);
-        }
-        currentPage = riot.mount('div#main', 'reports')[0];
+    route(getRiotUrl('reports'), function() {
+        goRoute('reports');
     });
+
+    // Fix these routes to use the config url system or avoid the router
+    route('/api..', function() {
+        window.location = '/api/';
+    });
+    route('/admin..', function() {
+        window.location = '/admin/';
+    });
+    route('/logout..', function() {
+        window.location = '/logout/';
+    });
+    route('/', function() {
+        window.location = '/';
+    });
+
+    // Routing start
     route.base('/');
     route.start(true);
-    $('.routable').each(function(i, e) {
-        $(this).on('click', function(e) {
-            e.preventDefault();
-            route($(this).attr('href'));
-        });
-    });
 });
