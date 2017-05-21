650abe63c4ef4cd6150415926d415c7d8c3822d6
Isaac Bythewood <isaac@bythewood.me>
2017-05-21 15:20:40 -0400

    Fix hotkeys for SPA

--- static_src/scripts/hotkeys.js
+++ static_src/scripts/hotkeys.js
@@ -1,16 +1,18 @@
-// TODO: Pull locations from django's url system
 // TODO: Also don't use hotkeys 1, 2, 3 + alt, some systems use that to tab
 $(document).keypress(function(e) {
     if (e.altKey) {
         switch (e.which) {
         case 49:
-            window.location = '/clients/';
+            route(timestrapConfig.CORE_URLS.ENTRIES);
             break;
         case 50:
-            window.location = '/entries/';
+            route(timestrapConfig.CORE_URLS.CLIENTS);
             break;
         case 51:
-            window.location = '/reports';
+            route(timestrapConfig.CORE_URLS.TASKS);
+            break;
+        case 52:
+            route(timestrapConfig.CORE_URLS.REPORTS);
             break;
         }
     }
