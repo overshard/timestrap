977896152593ee0e6bbeeb31092a0d03e6577765
Christopher Charbonneau Wells <chris@chrxs.net>
2017-05-31 16:01:11 -0400

    List current clients in Clients vue.

--- timestrap/static_src/tags/clients.vue
+++ timestrap/static_src/tags/clients.vue
@@ -36,6 +36,33 @@
             </button>
         </div>
     </form>
+
+    <div class="client-rows rounded">
+        <div v-for="client in clients" class="row py-2 bg-faded rounded mb-2">
+            <div class="col-6 d-flex align-items-center">
+                <a class="client-view-projects text-primary font-weight-bold"
+                   v-on:click="showProjects">
+                    <i class="fa fa-chevron-circle-up small mr-2" aria-hidden="true"></i>
+                    <span class="mb-1">{{ client.name }}</span>
+                </a>
+                <!--<span class="text-primary font-weight-bold">{{ client.name }}</span>-->
+            </div>
+            <div class="col-2 d-flex align-items-center">
+                <i class="fa fa-clock-o text-muted mr-2" aria-hidden="true"></i>
+                <span class="mb-1">{{ client.total_duration }}</span>
+            </div>
+            <div class="col-2 d-flex align-items-center">
+                <i class="fa fa-list text-muted mr-2" aria-hidden="true"></i>
+                <span class="mb-1">{{ client.total_projects }}</span>
+            </div>
+            <div class="col-2">
+                <button name="client-change"
+                        class="btn btn-warning btn-sm w-100">
+                    Edit
+                </button>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -52,6 +79,9 @@ export default {
             quickFetch(url).then(data => {
                 this.clients = data;
             }).catch(error => console.log(error));
+        },
+        showProjects() {
+            console.log(this);
         },
         submitClient() {
             console.log(this);
