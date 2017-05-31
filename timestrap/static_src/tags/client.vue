785a48628ed0868986527a50890c907895ab824b
Christopher Charbonneau Wells <chris@chrxs.net>
2017-05-31 16:31:46 -0400

    Separate Client in to a distinct vue.

---
+++ timestrap/static_src/tags/client.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="row py-2 bg-faded rounded mb-2">
+    <div class="col-6 d-flex align-items-center">
+        <a class="client-view-projects text-primary font-weight-bold"
+           v-on:click="showProjects">
+            <i class="fa fa-chevron-circle-up small mr-2" aria-hidden="true"></i>
+            <span class="mb-1">{{ client.name }}</span>
+        </a>
+        <!--<span class="text-primary font-weight-bold">{{ client.name }}</span>-->
+    </div>
+    <div class="col-2 d-flex align-items-center">
+        <i class="fa fa-clock-o text-muted mr-2" aria-hidden="true"></i>
+        <span class="mb-1">{{ client.total_duration }}</span>
+    </div>
+    <div class="col-2 d-flex align-items-center">
+        <i class="fa fa-list text-muted mr-2" aria-hidden="true"></i>
+        <span class="mb-1">{{ client.total_projects }}</span>
+    </div>
+    <div class="col-2">
+        <button name="client-change"
+                class="btn btn-warning btn-sm w-100">
+            Edit
+        </button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['client'],
+    data() {
+        return {
+        };
+    },
+    methods: {
+        showProjects() {
+            console.log(this);
+        }
+    },
+    mounted() {
+    }
+}
+</script>
--- timestrap/static_src/tags/clients.vue
+++ timestrap/static_src/tags/clients.vue
@@ -37,36 +37,19 @@
         </div>
     </form>
 
-    <div class="client-rows rounded">
-        <div v-for="client in clients" class="row py-2 bg-faded rounded mb-2">
-            <div class="col-6 d-flex align-items-center">
-                <a class="client-view-projects text-primary font-weight-bold"
-                   v-on:click="showProjects">
-                    <i class="fa fa-chevron-circle-up small mr-2" aria-hidden="true"></i>
-                    <span class="mb-1">{{ client.name }}</span>
-                </a>
-                <!--<span class="text-primary font-weight-bold">{{ client.name }}</span>-->
-            </div>
-            <div class="col-2 d-flex align-items-center">
-                <i class="fa fa-clock-o text-muted mr-2" aria-hidden="true"></i>
-                <span class="mb-1">{{ client.total_duration }}</span>
-            </div>
-            <div class="col-2 d-flex align-items-center">
-                <i class="fa fa-list text-muted mr-2" aria-hidden="true"></i>
-                <span class="mb-1">{{ client.total_projects }}</span>
-            </div>
-            <div class="col-2">
-                <button name="client-change"
-                        class="btn btn-warning btn-sm w-100">
-                    Edit
-                </button>
-            </div>
-        </div>
+    <client v-for="(client, index) in clients"
+            v-bind:client="client"
+            v-bind:index="index"
+            v-bind:key="client.id">
+    </client>
+
     </div>
 </div>
 </template>
 
 <script>
+const Client = require('./client.vue');
+
 export default {
     data() {
         return {
@@ -80,15 +63,15 @@ export default {
                 this.clients = data;
             }).catch(error => console.log(error));
         },
-        showProjects() {
-            console.log(this);
-        },
         submitClient() {
             console.log(this);
         }
     },
     mounted() {
         return this.getClients();
+    },
+    components: {
+        Client
     }
 }
 </script>
