7a79f1c6906e5a6f9f846b3f806dc4bcd2458d1f
Isaac Bythewood <isaac@bythewood.me>
2017-06-09 14:56:55 -0400

    Sidebar improvements

--- timestrap/static_src/components/app.vue
+++ timestrap/static_src/components/app.vue
@@ -1,68 +1,66 @@
 <template>
-<div id="app" class="container-fluid">
-    <div class="row">
-        <div class="sidebar-row bg-primary col-xl-2 col-lg-3 col-md-4 col-sm-12">
-            <div class="sidebar">
-                <router-link class="display-4" v-bind:to="timesheet">
-                    Timestrap
+<div id="app">
+    <div class="sidebar bg-primary">
+        <div class="branding">
+            <router-link class="display-4" v-bind:to="timesheet">
+                Timestrap
+            </router-link>
+        </div>
+        <ul id="nav-app" class="nav flex-column">
+            <li class="nav-item">
+                <router-link id="nav-app-timesheet" class="nav-link" v-bind:to="timesheet">
+                    <i class="fa fa-clock-o mr-1" aria-hidden="true"></i>
+                    Timesheet
                 </router-link>
-                <ul id="nav-app" class="nav flex-column">
-                    <li class="nav-item">
-                        <router-link id="nav-app-timesheet" class="nav-link" v-bind:to="timesheet">
-                            <i class="fa fa-clock-o mr-1" aria-hidden="true"></i>
-                            Timesheet
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link id="nav-app-clients" class="nav-link" v-bind:to="clients">
-                            <i class="fa fa-address-book mr-1" aria-hidden="true"></i>
-                            Clients
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link id="nav-app-tasks" class="nav-link" v-bind:to="tasks">
-                            <i class="fa fa-tasks mr-1" aria-hidden="true"></i>
-                            Tasks
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link id="nav-app-reports" class="nav-link" v-bind:to="reports">
-                            <i class="fa fa-book mr-1" aria-hidden="true"></i>
-                            Reports
-                        </router-link>
-                    </li>
-                    <timer />
-                    <li class="nav-item dropup col-xl-2 col-lg-3 col-md-4 col-sm-12">
-                        <a class="nav-link dropdown-toggle"
-                            data-toggle="dropdown">
-                            <img v-bind:src="global.user.gravatar_url"
-                                width="30"
-                                height="30"
-                                class="mr-1" />
-                            {{ username }}
-                        </a>
-                        <div class="dropdown-menu">
-                            <a id="nav-admin-api"
-                               class="dropdown-item"
-                               target="_blank"
-                               v-bind:href="api">API Browser</a>
-                            <a id="nav-admin-admin"
-                               class="dropdown-item"
-                               target="_blank"
-                               v-bind:href="admin">Admin</a>
-                            <a id="nav-admin-logout"
-                               class="dropdown-item"
-                               target="_blank"
-                               v-bind:href="logout">Logout</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="my-3 col-9 col-xl-10 col-lg-9 col-md-8 col-sm-12">
-            <router-view v-bind:id="['component-' + $route.name]"
-                         class="view"></router-view>
-        </div>
+            </li>
+            <li class="nav-item">
+                <router-link id="nav-app-clients" class="nav-link" v-bind:to="clients">
+                    <i class="fa fa-address-book mr-1" aria-hidden="true"></i>
+                    Clients
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link id="nav-app-tasks" class="nav-link" v-bind:to="tasks">
+                    <i class="fa fa-tasks mr-1" aria-hidden="true"></i>
+                    Tasks
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link id="nav-app-reports" class="nav-link" v-bind:to="reports">
+                    <i class="fa fa-book mr-1" aria-hidden="true"></i>
+                    Reports
+                </router-link>
+            </li>
+            <timer />
+            <li class="nav-item dropup">
+                <a class="nav-link dropdown-toggle"
+                    data-toggle="dropdown">
+                    <img v-bind:src="global.user.gravatar_url"
+                        width="30"
+                        height="30"
+                        class="mr-1" />
+                    {{ username }}
+                </a>
+                <div class="dropdown-menu">
+                    <a id="nav-admin-api"
+                        class="dropdown-item"
+                        target="_blank"
+                        v-bind:href="api">API Browser</a>
+                    <a id="nav-admin-admin"
+                        class="dropdown-item"
+                        target="_blank"
+                        v-bind:href="admin">Admin</a>
+                    <a id="nav-admin-logout"
+                        class="dropdown-item"
+                        target="_blank"
+                        v-bind:href="logout">Logout</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="content">
+        <router-view v-bind:id="['component-' + $route.name]"
+                        class="view"></router-view>
     </div>
 </div>
 </template>
