a3d9fbe47b14aee5d283e2a0aed272546797cd0f
Christopher Charbonneau Wells <chris@chrxs.net>
2017-06-19 16:28:20 -0400

    Check for resolved $user and $perms objects before routing.

--- timestrap/static_src/plugins/permissions.js
+++ timestrap/static_src/plugins/permissions.js
@@ -1,8 +1,7 @@
 // Enable sending and receiving form the django-rest-framework API.
 module.exports = {
     install: function(Vue, options) {
-        Vue.prototype.$perms = {};
-        Vue.prototype.$quickFetch(timestrapConfig.API_URLS.PERMISSIONS).then(data => {
+        Vue.prototype.$perms = Vue.prototype.$quickFetch(timestrapConfig.API_URLS.PERMISSIONS).then(data => {
             let perms = Object;
             for (let i = 0; i < data.length; i++) {
                 perms[data[i].codename] = data[i];
