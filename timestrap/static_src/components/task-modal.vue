e4333690e30d3af3f4a5959f23763ffa2abc2e59
Christopher Charbonneau Wells <chris@chrxs.net>
2017-06-29 12:30:04 -0400

    Make Modal an abstract component.

--- timestrap/static_src/components/task-modal.vue
+++ timestrap/static_src/components/task-modal.vue
@@ -1,65 +1,48 @@
 <template>
-<transition name="modal">
-    <div class="modal-mask">
-        <div class="modal-wrapper">
-            <div class="modal-container">
-                <form id="task-add"
-                   class="modal-content"
-                   v-on:submit.prevent
-                   v-on:submit="submitTask">
-                <div class="modal-header">
-                    <slot name="header">
-                        <h5 class="modal-title">
-                        <i class="fa fa-tasks mr-1" aria-hidden="true"></i>
-                        New Task
-                        </h5>
-                    </slot>
-                </div>
+<modal>
+    <h5 slot="header">
+        <i class="fa fa-tasks mr-1" aria-hidden="true"></i>New Task
+    </h5>
 
-                <div class="modal-body">
-                    <slot name="body">
-                        <div class="form-group">
-                            <input name="task-name"
-                                   placeholder="Task Name"
-                                   type="text"
-                                   class="form-control"
-                                   v-model.trim="name"
-                                   required />
-                        </div>
-                        <div class="form-group">
-                            <input name="task-hourly-rate"
-                                   placeholder="Hourly Rate"
-                                   type="text"
-                                   class="form-control"
-                                   v-model.number="hourly_rate"
-                                   required />
-                        </div>
-                    </slot>
-                </div>
-
-                <div class="modal-footer">
-                    <slot name="footer">
-                        <button name="task-add-cancel"
-                                type="button"
-                                class="btn btn-secondary"
-                                @click="$emit('close')">
-                            Close
-                        </button>
-                        <button name="task-add-submit"
-                                type="submit"
-                                class="btn btn-primary">
-                            Add Task
-                        </button>
-                    </slot>
-                </div>
-                </form>
-            </div>
+    <div slot="body">
+        <div class="form-group">
+        <input name="task-name"
+               placeholder="Task Name"
+               type="text"
+               class="form-control"
+               v-model.trim="name"
+               required />
+        </div>
+        <div class="form-group">
+            <input name="task-hourly-rate"
+                   placeholder="Hourly Rate"
+                   type="text"
+                   class="form-control"
+                   v-model.number="hourly_rate"
+                   required />
         </div>
     </div>
-</transition>
+
+    <div slot="footer">
+        <button name="task-add-cancel"
+                type="button"
+                class="btn btn-secondary"
+                @click="$emit('close')">
+            Close
+        </button>
+        <button name="task-add-submit"
+                type="submit"
+                class="btn btn-primary"
+                @click="submitTask">
+            Add Task
+        </button>
+    </div>
+</modal>
 </template>
 
 <script>
+const Modal = require('./modal.vue');
+
 export default {
     props: ['task'],
     data() {
@@ -81,6 +64,9 @@ export default {
                 this.$emit('close');
             }).catch(error => console.log(error));
         }
+    },
+    components: {
+        Modal
     }
 };
 </script>
