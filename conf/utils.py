5531ca95faa073ade4e88efe678cdb1caa2afa36
Christopher Charbonneau Wells <chris@chrxs.net>
2017-07-17 09:31:13 -0400

    Fall back on settings.SITE_ID when no Site is found in the request.

--- conf/utils.py
+++ conf/utils.py
@@ -2,12 +2,18 @@ from __future__ import division
 
 from .middleware import current_request
 
+from django.conf import settings
+
 
 def current_site_id():
     """
-    Return the ID of the current Site.
+    Return the ID of the current Site by checking for a Site in the current
+    request and falling back on the default ID in Django settings.
     """
     request = current_request()
     site = getattr(request, 'site', None)
-    site_id = getattr(site, 'id', None)
+    if site:
+        site_id = getattr(site, 'id', None)
+    else:
+        site_id = settings.SITE_ID
     return site_id
