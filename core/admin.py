43decfca4faf7f75da2c2de49c7ad165fdf8485b
Christopher Charbonneau Wells <chris@chrxs.net>
2017-07-22 08:02:11 -0400

    Add Tasks to core admin and include site/s in model filters.

--- core/admin.py
+++ core/admin.py
@@ -5,30 +5,22 @@ from django.contrib import admin
 
 from import_export import resources
 
-from .models import Client, Project, Entry, Invoice
+from .models import Client, Entry, Invoice, Project, Task
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'invoice_email', 'payment_id', 'archive',)
     list_editable = ('archive',)
-    list_filter = ('archive',)
+    list_filter = ('archive', 'sites')
     search_fields = ('name',)
-
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'client', 'archive',)
-    list_editable = ('archive',)
-    list_filter = ('client', 'archive',)
-    search_fields = ('name', 'client',)
 
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('project', 'user', 'date', 'duration',)
     list_editable = ('date', 'duration',)
-    list_filter = ('project', 'project__client', 'user', 'date',)
+    list_filter = ('project', 'project__client', 'user', 'date', 'site',)
     search_fields = ('project', 'project__client', 'user', 'note',)
     fieldsets = (
         (None, {
@@ -54,6 +46,21 @@ class EntryResource(resources.ModelResource):
 class InvoiceAdmin(admin.ModelAdmin):
     list_display = ('client', 'created', 'paid', 'transaction_id',)
     list_editable = ('paid', 'transaction_id',)
-    list_filter = ('client', 'paid',)
+    list_filter = ('client', 'paid', 'site',)
     search_fields = ('client', 'transaction_id',)
     filter_horizontal = ('entries',)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'client', 'archive',)
+    list_editable = ('archive',)
+    list_filter = ('client', 'archive',)
+    search_fields = ('name', 'client',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'hourly_rate',)
+    list_filter = ('sites',)
+    search_fields = ('name',)
