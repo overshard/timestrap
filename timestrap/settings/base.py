a82e4811c045ec2a239b02a92964bf337a04c4f1
Isaac Bythewood <isaac@bythewood.me>
2017-07-22 17:54:56 -0400

    Lower default page size to improve timesheet load times

--- timestrap/settings/base.py
+++ timestrap/settings/base.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'api.permissions.TimestrapDjangoModelPermissions'
     ],
-    'PAGE_SIZE': 50,
+    'PAGE_SIZE': 25,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
