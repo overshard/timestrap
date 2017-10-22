37c0069405a4775538e6b24cb8581e522ae272f8
Isaac Bythewood <isaac@bythewood.me>
2017-10-22 12:34:40 -0400

    Add icons to entries

--- timestrap/static_src/components/entry.vue
+++ timestrap/static_src/components/entry.vue
@@ -35,23 +35,29 @@
 
     <template v-else>
         <div class="col-sm-3 client-project">
-            <div class="text-muted small">
+            <div class="small">
+                <i class="fa fa-address-book text-muted mr-2" aria-hidden="true"></i>
                 {{ project_details.client_details.name }}
             </div>
+            <i class="fa fa-briefcase text-muted mr-1" aria-hidden="true"></i>
             {{ project_details.name }}
         </div>
-        <div v-bind:class="['d-flex', 'flex-column', 'align-self-end', 'tasks', 'col-sm-5']">
-            <div class="text-muted small"
-                 v-if="task">
+        <div v-bind:class="['d-flex', 'flex-column', 'align-self-end', 'tasks', 'col-sm-7']">
+            <div class="small" v-if="task">
+                <i class="fa fa-tasks text-muted mr-2" aria-hidden="true"></i>
                 {{ task_details.name }}
             </div>
-            {{ note }}
+            <div class="entry-note">
+                <i class="fa fa-comment text-muted mr-1" aria-hidden="true"></i>
+                {{ note }}
+            </div>
         </div>
-        <div class="col-sm-2 d-flex align-self-center justify-content-end display-4 duration">
+        <div class="col-sm-1 d-flex align-self-center display-4 duration">
+            <i class="fa fa-clock-o text-muted mr-2" aria-hidden="true"></i>
             {{ duration }}
         </div>
         <template v-if="editable">
-            <div class="col-sm-2 d-flex align-self-center justify-content-end">
+            <div class="col-sm-1 d-flex align-self-center justify-content-end">
                 <template v-if="this.$perms.change_entry || this.$perms.delete_entry">
                     <button name="entry-menu"
                             class="btn btn-faded btn-sm btn-icon dropdown-toggle"
