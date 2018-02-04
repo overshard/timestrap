3570caee839752638fd051f42793fa0eea810753
Isaac Bythewood <isaac@bythewood.me>
2018-02-04 19:28:30 +0000

    Add option to gulp run on public port 80

--- gulpfile.js/tasks/default.js
+++ gulpfile.js/tasks/default.js
@@ -4,14 +4,16 @@ const spawn = require('child_process').spawn;
 
 
 gulp.task('default', ['build', 'watch'], function(cb) {
+    const command = [ 'run', 'python', 'manage.py', 'runserver' ];
+    const args = process.argv;
+
+    if (args[3] == '--public') {
+        command.push('0.0.0.0:80');
+    }
+
     spawn(
         'pipenv',
-        [
-            'run',
-            'python',
-            'manage.py',
-            'runserver'
-        ],
+        command,
         {
             stdio: 'inherit'
         }
