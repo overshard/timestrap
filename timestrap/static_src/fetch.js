fc99cbf6ec7c3f9315b7c17d7477cdb19923e27f
Isaac Bythewood <isaac@bythewood.me>
2018-06-26 22:51:31 +0000

    Swap to axios for tasks

---
+++ timestrap/static_src/fetch.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+
+export default axios.create({
+    xsrfCookieName: 'csrftoken',
+    xsrfHeaderName: 'X-CSRFToken',
+});
--- timestrap/static_src/store/modules/tasks.js
+++ timestrap/static_src/store/modules/tasks.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
+import fetch from '../../fetch';
+
 
 export default {
     namespaced: true,
@@ -20,24 +22,26 @@ export default {
     actions: {
         getTasks({commit}) {
             commit('setLoading', true, {root: true});
-            Vue.prototype.$quickFetch(timestrapConfig.API_URLS.TASKS).then(data => {
-                commit('setTasks', data);
+            fetch.get(timestrapConfig.API_URLS.TASKS).then(response => {
+                commit('setTasks', response.data);
                 commit('setLoading', false, {root: true});
-            }).catch(error => console.error(error));
+            }).catch(error => console.log(error));
+
         },
         createTask({commit}, task) {
-            Vue.prototype.$quickFetch(timestrapConfig.API_URLS.TASKS, 'post', task).then(data => {
-                commit('addTask', data);
+            fetch.post(timestrapConfig.API_URLS.TASKS, task).then(response => {
+                commit('addTask', response.data);
             }).catch(error => console.log(error));
         },
         editTask({commit, state}, task) {
-            Vue.prototype.$quickFetch(task.url, 'put', task).then(data => {
-                const index = state.all.findIndex(item => {return item.id === data.id});
-                commit('updateTask', {index: index, task:data});
+            fetch.put(task.url, task).then(response => {
+                const index = state.all.findIndex(item => {return item.id === response.data.id});
+                commit('updateTask', {index: index, task: response.data});
             }).catch(error => console.log(error));
+
         },
         deleteTask({commit, state}, index) {
-            Vue.prototype.$quickFetch(state.all[index].url, 'delete').then(data => {
+            fetch.delete(state.all[index].url).then(response => {
                 commit('removeTask', index);
             }).catch(error => console.log(error));
         },
