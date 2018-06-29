4d04e7a810cc395246cb966d7e92211106833000
Isaac Bythewood <isaac@bythewood.me>
2018-06-29 01:59:36 +0000

    Cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
-const webpack = require('webpack')
-
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 
 module.exports = {
@@ -10,52 +7,39 @@ module.exports = {
         rules: [
             {
                 test: /\.vue$/,
-                loader: 'vue-loader'
+                loader: 'vue-loader',
             },
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                exclude: /node_modules/
+                exclude: /node_modules/,
             },
             {
                 test: /\.scss$/,
                 loaders: [
                     'style-loader',
                     'css-loader',
-                    'sass-loader'
-                ]
+                    'sass-loader',
+                ],
             },
             {
                 test: /\.(png|jpg|gif|svg)$/,
                 loader: 'file-loader',
                 options: {
-                    name: '[name].[ext]?[hash]'
-                }
-            }
-        ]
+                    name: '[name].[ext]?[hash]',
+                },
+            },
+        ],
     },
     plugins: [
         new VueLoaderPlugin(),
-        new UglifyJsPlugin({
-            uglifyOptions: {
-                sourceMap: false,
-                compress: {
-                    warnings: false
-                }
-            }
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production'),
-            }
-        }),
     ],
     resolve: {
         alias: {
-            'vue$': 'vue/dist/vue.esm.js'
-        }
+            'vue$': 'vue/dist/vue.esm.js',
+        },
     },
     performance: {
-        hints: false
-    }
+        hints: false,
+    },
 }
