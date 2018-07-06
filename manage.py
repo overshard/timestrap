e3e201281d04270f70614f85d1a1368891c82410
Isaac Bythewood <isaac@bythewood.me>
2018-07-05 23:31:08 -0400

    Update manage.py to the latest from django

--- manage.py
+++ manage.py
@@ -1,26 +1,18 @@
-#!/usr/bin/env python
 import os
 import sys
 
-if __name__ == "__main__":
-    sys.dont_write_bytecode = True
+
+if __name__ == '__main__':
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-        "timestrap.settings.development"
+        'DJANGO_SETTINGS_MODULE',
+        'timestrap.settings.development'
     )
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django  # noqa: F401
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
