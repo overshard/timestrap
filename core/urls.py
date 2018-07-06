509c6243a02c050eacbc05237398da347cf5ff8c
Isaac Bythewood <isaac@bythewood.me>
2018-07-05 23:41:32 -0400

    Fix bug where root URL would sometimes not work

--- core/urls.py
+++ core/urls.py
@@ -5,16 +5,7 @@ from django.urls import reverse_lazy
 from . import views
 
 
-# Instead of using a wildcard for our app views we insert them one at a time
-# for naming purposes. This is so that users can change urls if they want and
-# the change will propagate throughout the site.
 urlpatterns = [
-    path(
-        '',
-        RedirectView.as_view(url=reverse_lazy('timesheet'), permanent=False),
-        name='dashboard',
-    ),
-
     path(
         'reports/export/',
         views.ReportExportView.as_view(),
@@ -25,4 +16,9 @@ urlpatterns = [
     path('clients/', views.AppView.as_view(), name='clients'),
     path('tasks/', views.AppView.as_view(), name='tasks'),
     path('reports/', views.AppView.as_view(), name='reports'),
+    path(
+        '',
+        RedirectView.as_view(url=reverse_lazy('timesheet'), permanent=False),
+        name='dashboard',
+    ),
 ]
