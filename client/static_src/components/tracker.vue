20b43dfc051c63ce77796bcd06c6b20efe3afa7a
Isaac Bythewood <isaac@bythewood.me>
2019-11-16 21:55:51 +0000

    Add second row to tracker to adjust clutter

--- client/static_src/components/tracker.vue
+++ client/static_src/components/tracker.vue
@@ -1,128 +1,119 @@
 <template>
-<div class="bg-light py-2">
-  <form
-    id="tracker"
-    class="container-fluid"
-    @submit.prevent="submitEntry">
-    <div class="row">
-
-      <div
-        class="col-sm-2"
-        @keyup.enter.prevent>
-        <selector
-          id="tracker-client"
-          v-model="client"
-          :options="clients"
-          :selected="client"
-          placeholder="Client"
+  <div class="bg-light py-2">
+    <form id="tracker" class="container-fluid" @submit.prevent="submitEntry">
+      <div class="row">
+        <div class="col-sm-3 px-2" @keyup.enter.prevent>
+          <selector
+            id="tracker-client"
+            v-model="client"
+            :options="clients"
+            :selected="client"
+            placeholder="Client"
           />
-      </div>
+        </div>
 
-      <div
-        class="col-sm-2"
-        @keyup.enter.prevent>
-        <selector
-          id="tracker-project"
-          v-model="project"
-          :options="projects"
-          :selected="project"
-          placeholder="Project"
-          required/>
-      </div>
+        <div class="col-sm-3 px-2" @keyup.enter.prevent>
+          <selector
+            id="tracker-project"
+            v-model="project"
+            :options="projects"
+            :selected="project"
+            placeholder="Project"
+            required
+          />
+        </div>
 
-      <div
-        class="col-sm-2"
-        @keyup.enter.prevent>
-        <selector
-          id="tracker-task"
-          v-model="task"
-          :options="tasks"
-          :selected="task"
-          placeholder="Task"
-          required/>
-      </div>
+        <div class="col-sm-3 px-2" @keyup.enter.prevent>
+          <selector
+            id="tracker-task"
+            v-model="task"
+            :options="tasks"
+            :selected="task"
+            placeholder="Task"
+            required
+          />
+        </div>
 
-      <div class="col-sm-2">
-        <input
-          id="tracker-note"
-          v-model="note"
-          class="form-control w-100 shadow-sm"
-          placeholder="Note"
-          type="text">
-      </div>
-
-      <div class="col-sm-4">
-
-        <div class="row">
-          <div class="col-sm-4">
-            <datepicker
-              v-model="date"
-              id="tracker-date"
-              type="text"
-              class="form-control date-input text-center"
-              placeholder="Date"
-            />
-          </div>
-
-          <div class="col-sm-3">
-            <input
-              id="tracker-duration"
-              v-model="duration"
-              class="form-control text-right font-weight-bold w-100 shadow-sm"
-              placeholder="0:00"
-              type="text"
-              required>
-          </div>
-
-          <div class="col-sm-5">
-            <button
-              v-if="!running && !duration"
-              class="btn btn-success w-100"
-              @click.prevent.exact="toggle">
-              <icon :icon="['fas', 'play']" class="mr-1"/>
-              Start
-            </button>
-            <button
-              v-else-if="running"
-              class="btn btn-danger w-100"
-              @click.prevent.exact="toggle">
-              <icon :icon="['fas', 'stop']" class="mr-1"/>
-              Stop ({{ seconds }})
-            </button>
-            <button
-              v-else-if="!running && duration"
-              :disabled="!canSubmit"
-              id="tracker-submit"
-              class="btn btn-info w-100"
-              type="submit">
-              <icon :icon="['fas', 'plus']" class="mr-1"/>
-              Add
-            </button>
-          </div>
-
+        <div class="col-sm-3 px-2">
+          <datepicker
+            v-model="date"
+            id="tracker-date"
+            type="text"
+            class="form-control date-input text-center"
+            placeholder="Date"
+          />
         </div>
       </div>
 
-    </div>
-  </form>
-</div>
+      <div class="row mt-2">
+        <div class="col-sm-6 px-2">
+          <input
+            id="tracker-note"
+            v-model="note"
+            class="form-control w-100 shadow-sm"
+            placeholder="Note"
+            type="text"
+          />
+        </div>
+
+        <div class="col-sm-3 px-2">
+          <input
+            id="tracker-duration"
+            v-model="duration"
+            class="form-control text-right font-weight-bold w-100 shadow-sm"
+            placeholder="0:00"
+            type="text"
+            required
+          />
+        </div>
+
+        <div class="col-sm-3 px-2">
+          <button
+            v-if="!running && !duration"
+            class="btn btn-success w-100"
+            @click.prevent.exact="toggle"
+          >
+            <icon :icon="['fas', 'play']" class="mr-1" />
+            Start
+          </button>
+          <button
+            v-else-if="running"
+            class="btn btn-danger w-100"
+            @click.prevent.exact="toggle"
+          >
+            <icon :icon="['fas', 'stop']" class="mr-1" />
+            Stop ({{ seconds }})
+          </button>
+          <button
+            v-else-if="!running && duration"
+            :disabled="!canSubmit"
+            id="tracker-submit"
+            class="btn btn-info w-100"
+            type="submit"
+          >
+            <icon :icon="['fas', 'plus']" class="mr-1" />
+            Add
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
-
 <script>
-import {mapGetters, mapActions} from 'vuex';
+import { mapGetters, mapActions } from "vuex";
 
-import Selector from './selector.vue';
-import Datepicker from './datepicker.vue';
+import Selector from "./selector.vue";
+import Datepicker from "./datepicker.vue";
 
-function getFromLocalStorage(key, defaultValue){
-  if(!key in window.localStorage){
+function getFromLocalStorage(key, defaultValue) {
+  if (!key in window.localStorage) {
     return defaultValue;
   }
-  if(defaultValue === null && window.localStorage[key] === "null"){
+  if (defaultValue === null && window.localStorage[key] === "null") {
     return null;
   }
-  if(defaultValue === false){
+  if (defaultValue === false) {
     return window.localStorage[key] === "true";
   }
   return window.localStorage[key];
@@ -131,7 +122,7 @@ function getFromLocalStorage(key, defaultValue){
 export default {
   components: {
     Selector,
-    Datepicker,
+    Datepicker
   },
   data() {
     return {
@@ -140,63 +131,66 @@ export default {
       project: getFromLocalStorage("tracker_project", null),
       note: getFromLocalStorage("tracker_note", null),
       duration: null,
-      datetimeStart: "tracker_start" in window.localStorage ? new Date(Number(window.localStorage.tracker_start)) : null,
+      datetimeStart:
+        "tracker_start" in window.localStorage
+          ? new Date(Number(window.localStorage.tracker_start))
+          : null,
       datetimeEnd: null,
-      date: this.$moment().format('YYYY-MM-DD'),
+      date: this.$moment().format("YYYY-MM-DD"),
 
       running: getFromLocalStorage("tracker_running", false),
       total: 0,
-      seconds: '0',
+      seconds: "0"
     };
   },
   computed: {
     ...mapGetters({
-      clients: 'clients/getSelectClients',
-      tasks: 'tasks/getSelectTasks',
-      projectsAll: 'projects/getSelectProjects',
-      projectsClient: 'projects/getSelectProjectsForClient',
-      users: 'users/getSelectUsers',
-      getProject: 'projects/getProject',
+      clients: "clients/getSelectClients",
+      tasks: "tasks/getSelectTasks",
+      projectsAll: "projects/getSelectProjects",
+      projectsClient: "projects/getSelectProjectsForClient",
+      users: "users/getSelectUsers",
+      getProject: "projects/getProject"
     }),
-    projects(){
-      if(!this.client) {
+    projects() {
+      if (!this.client) {
         return this.projectsAll;
-      }else{
+      } else {
         return this.projectsClient(this.client);
       }
     },
-    canSubmit(){
+    canSubmit() {
       return this.project && this.task;
     }
   },
   watch: {
-    client(){
-      if(this.project){
+    client() {
+      if (this.project) {
         const p = this.getProject();
-        if(p.client !== this.client){
+        if (p.client !== this.client) {
           this.project = null;
         }
       }
     },
-    project(){
+    project() {
       window.localStorage.tracker_project = this.project;
     },
-    task(){
+    task() {
       window.localStorage.tracker_task = this.task;
     },
-    note(){
+    note() {
       window.localStorage.tracker_note = this.note;
-    },
+    }
   },
-  created(){
-    if(this.running){
+  created() {
+    if (this.running) {
       this.tick();
       this.start();
     }
   },
   methods: {
     ...mapActions({
-      createEntry: 'entries/createEntry',
+      createEntry: "entries/createEntry"
     }),
     submitEntry() {
       this.createEntry({
@@ -207,7 +201,7 @@ export default {
         datetime_start: this.datetimeStart,
         datetime_end: this.datetimeEnd,
         user: timestrapConfig.USER.URL,
-        date: this.date//this.$moment().format('YYYY-MM-DD'),
+        date: this.date //this.$moment().format('YYYY-MM-DD'),
       });
       this.reset();
       this.note = null;
@@ -215,17 +209,19 @@ export default {
     reset() {
       this.duration = null;
       this.total = 0;
-      this.seconds = '0';
+      this.seconds = "0";
     },
     tick() {
       this.total = Math.floor(
         (Date.now() - this.datetimeStart.valueOf()) / 1000
       );
-      this.seconds = this.total % 3600 % 60;
-      this.duration = this.$moment.duration(this.total, 'seconds').format('h:mm', {trim: false});
-      document.title = this.duration + ' — ' + timestrapConfig.SITE.NAME;
+      this.seconds = (this.total % 3600) % 60;
+      this.duration = this.$moment
+        .duration(this.total, "seconds")
+        .format("h:mm", { trim: false });
+      document.title = this.duration + " — " + timestrapConfig.SITE.NAME;
     },
-    start(){
+    start() {
       this.interval = setInterval(this.tick, 1000, this);
     },
     toggle() {
@@ -242,9 +238,9 @@ export default {
         window.localStorage.tracker_start = null;
         clearInterval(this.interval);
         if (this.total < 60) this.reset();
-        document.title = 'Application — ' + timestrapConfig.SITE.NAME;
+        document.title = "Application — " + timestrapConfig.SITE.NAME;
       }
-    },
-  },
+    }
+  }
 };
 </script>
